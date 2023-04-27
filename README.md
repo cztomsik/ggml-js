ba41d043fe76cc213758cfbe88fd7919e214cb8b
Kamil Tomsik <info@tomsik.cz>
2023-04-27 13:32:08 +0200

    WIP: rwkv

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # ggml-js
+
 JavaScript bindings for the [ggml](https://github.com/ggerganov/ggml) library.
 
 ## Installation
+
 You can install ggml-js via npm:
 
 ```bash
@@ -9,37 +11,39 @@ npm install ggml-js
 ```
 
 ## Usage
+
 Here's an example of how to use ggml-js in your JavaScript code:
 
 ```js
-import { Context } from 'ggml-js';
+import { Context, F } from 'ggml-js'
 
 // Initialize the context
-const ctx = Context.init();
+const ctx = Context.init()
 
 // Create new 1D tensors a and b
-const a = ctx.newTensor1D('f32', 1);
-const b = ctx.newTensor1D('f32', 1);
+const a = ctx.newTensor1D('f32', 1)
+const b = ctx.newTensor1D('f32', 1)
 
 // Perform element-wise multiplication of a and b
-const ab = a.mul(b);
+const ab = F.mul(a, b)
 
 // Build the forward computation graph
-const graph = ab.buildForward();
+const graph = ctx.buildForward(ab)
 
 // Print the graph structure
-graph.print();
+graph.print()
 
 // Set values for tensors a and b
-a.set(0, 1.5);
-b.set(0, 2);
+a.set(0, 1.5)
+b.set(0, 2)
 
 // Compute the graph
-graph.compute();
+graph.compute()
 
 // Print the result of the multiplication
-console.log(ab.get(0));
+console.log(ab.get(0))
 ```
 
 ## License
+
 This project is licensed under the MIT License.
--- examples/rwkv.js
+++ examples/rwkv.js
@@ -1,24 +1,28 @@
 // based on https://johanwind.github.io/2023/03/23/rwkv_details.html
 // and https://github.com/BlinkDL/ChatRWKV/blob/main/RWKV_in_150_lines.py
+// and https://github.com/saharNooby/rwkv.cpp
+//
 // - download https://huggingface.co/BlinkDL/rwkv-4-pile-169m/blob/main/RWKV-4-Pile-169M-20220807-8023.pth
 // - run `python convert.py <file>` to generate `.safetensors` file
 // - run `node rwkv.js <file>` to run the model
 
-import { Context, Model, Module, Embedding, LayerNorm, Linear, F } from 'ggml-js'
+import { Context, LLM, Module, Embedding, LayerNorm, Linear, F } from 'ggml-js'
 
-// TODO: model.hparams?
 const [N_VOCAB, N_EMB, N_LAYER] = [50277, 768, 12]
 
-class RWKV extends Model {
+class RWKV extends LLM {
   [`blocks.0.ln0`] = new LayerNorm(this, N_EMB)
   emb = new Embedding(this, N_VOCAB, N_EMB)
   blocks = Array.from(Array(N_LAYER), _ => new Block(this))
   ln_out = new LayerNorm(this, N_EMB)
   head = new Linear(this, N_EMB, N_VOCAB, { bias: false })
+  #state = Array.from(Array(N_LAYER * 5), (_, i) =>
+    this.context.newTensor1D('f32', N_EMB).setAll(i % 5 === 3 ? -1e30 : 0)
+  )
 
-  forward(x) {
+  forward(x, updates = []) {
     x = this[`blocks.0.ln0`].forward(this.emb.forward(x))
-    this.blocks.forEach(b => (x = b.forward(x)))
+    x = this.blocks.reduce((x, block, i, _, o = i * 5) => block.forward(x, this.#state.slice(o, o + 5), updates), x)
     return this.head.forward(this.ln_out.forward(x))
   }
 }
@@ -28,20 +32,11 @@ class Block extends Module {
   att = new TimeMix(this)
   ln2 = new LayerNorm(this, N_EMB)
   ffn = new ChannelMix(this)
-  #prev = new State(this)
 
-  forward(x) {
-    x = F.add(x, this.att.forward(this.ln1.forward(x), this.#prev))
-    return F.add(x, this.ffn.forward(this.ln2.forward(x), this.#prev))
+  forward(x, state, updates) {
+    x = F.add(x, this.att.forward(this.ln1.forward(x), state, updates))
+    return F.add(x, this.ffn.forward(this.ln2.forward(x), state, updates))
   }
-}
-
-class State extends Module {
-  cx = this.context.newTensor1D('f32', N_EMB).setAll(0)
-  tx = this.context.newTensor1D('f32', N_EMB).setAll(0)
-  aa = this.context.newTensor1D('f32', N_EMB).setAll(0)
-  bb = this.context.newTensor1D('f32', N_EMB).setAll(-1e30)
-  pp = this.context.newTensor1D('f32', N_EMB).setAll(0)
 }
 
 class TimeMix extends Module {
@@ -55,29 +50,33 @@ class TimeMix extends Module {
   receptance = new Linear(this, N_EMB, N_EMB, { bias: false })
   output = new Linear(this, N_EMB, N_EMB, { bias: false })
 
-  forward(x, prev) {
-    const xk = F.add(F.mul(x, this.time_mix_k), F.mul(prev.tx, F.oneMinusX(this.time_mix_k)))
-    const xv = F.add(F.mul(x, this.time_mix_v), F.mul(prev.tx, F.oneMinusX(this.time_mix_v)))
-    const xr = F.add(F.mul(x, this.time_mix_r), F.mul(prev.tx, F.oneMinusX(this.time_mix_r)))
-    prev.tx = x
+  forward(x, [_, prev_x, aa, bb, pp], updates) {
+    const xk = F.add(F.mul(x, this.time_mix_k), F.mul(prev_x, F.oneMinusX(this.time_mix_k)))
+    const xv = F.add(F.mul(x, this.time_mix_v), F.mul(prev_x, F.oneMinusX(this.time_mix_v)))
+    const xr = F.add(F.mul(x, this.time_mix_r), F.mul(prev_x, F.oneMinusX(this.time_mix_r)))
     const r = F.sigmoid(this.receptance.forward(xr))
     const k = this.key.forward(xk)
     const v = this.value.forward(xv)
 
     let ww = F.add(this.time_first, k)
-    let qq = F.max(prev.pp, ww)
-    let e1 = F.exp(F.sub(prev.pp, qq))
+    let qq = F.max(pp, ww)
+    let e1 = F.exp(F.sub(pp, qq))
     let e2 = F.exp(F.sub(ww, qq))
-    const a = F.add(F.mul(e1, prev.aa), F.mul(e2, v))
-    const b = F.add(F.mul(e1, prev.bb), e2)
+    const a = F.add(F.mul(e1, aa), F.mul(e2, v))
+    const b = F.add(F.mul(e1, bb), e2)
     const wkv = F.div(a, b)
-    ww = F.add(prev.pp, this.time_decay)
+    ww = F.add(pp, this.time_decay)
     qq = F.max(ww, k)
     e1 = F.exp(F.sub(ww, qq))
     e2 = F.exp(F.sub(k, qq))
-    prev.aa = F.add(F.mul(e1, prev.aa), F.mul(e2, v))
-    prev.bb = F.add(F.mul(e1, prev.bb), e2)
-    prev.pp = qq
+
+    updates.push(
+      F.cpy(prev_x, x),
+      F.cpy(aa, F.add(F.mul(e1, aa), F.mul(e2, v))),
+      F.cpy(bb, F.add(F.mul(e1, bb), e2)),
+      F.cpy(pp, qq)
+    )
+
     return this.output.forward(F.mul(r, wkv))
   }
 }
@@ -89,11 +88,11 @@ class ChannelMix extends Module {
   receptance = new Linear(this, N_EMB, N_EMB, { bias: false })
   value = new Linear(this, 4 * N_EMB, N_EMB, { bias: false })
 
-  forward(x, prev) {
-    const k = this.key.forward(F.add(F.mul(x, this.time_mix_k), F.mul(prev.cx, F.oneMinusX(this.time_mix_k))))
-    const r = this.receptance.forward(F.add(F.mul(x, this.time_mix_r), F.mul(prev.cx, F.oneMinusX(this.time_mix_r))))
+  forward(x, [prev_x], updates) {
+    const k = this.key.forward(F.add(F.mul(x, this.time_mix_k), F.mul(prev_x, F.oneMinusX(this.time_mix_k))))
+    const r = this.receptance.forward(F.add(F.mul(x, this.time_mix_r), F.mul(prev_x, F.oneMinusX(this.time_mix_r))))
     const vk = this.value.forward(F.square(F.relu(k)))
-    prev.cx = x
+    updates.push(F.cpy(prev_x, x))
     return F.mul(F.sigmoid(r), vk)
   }
 }
@@ -104,33 +103,12 @@ const model = new RWKV(ctx)
 model.loadFromFile(process.argv[2])
 // model.print()
 
-// RNN only has one input
-const x = ctx.newTensor1D('i32', 1)
-
-// Prepare the graph
-const out = F.softmax(model.forward(x))
-const graph = ctx.buildForward(out)
-
 // push few tokens from from https://raw.githubusercontent.com/BlinkDL/RWKV-LM/main/RWKV-v4/20B_tokenizer.json
-x.set(0, 12092) // `Hello`
-graph.compute()
-x.set(0, 1533) // `Ġworld`
-graph.compute()
-x.set(0, 2) // `!`
-graph.compute()
-x.set(0, 831) // `ĠThis`
-graph.compute()
-x.set(0, 310) // `Ġis`
-graph.compute()
-x.set(0, 247) // `Ġa`
-graph.compute()
+const tokens = [`Hello`, `Ġworld`, `!`, `ĠThis`, `Ġis`, `Ġa`]
+const ids = [12092, 1533, 2, 831, 310, 247]
 
-// sample
-for (let i = 0; i < N_VOCAB; i++) {
-  const p = out.get(i)
-  if (p > 0.1) {
-    console.log('p', p, 'i', i)
-  }
+for (const predictions of model.generate(ids)) {
+  console.log(predictions)
 }
 
 console.log('done')
