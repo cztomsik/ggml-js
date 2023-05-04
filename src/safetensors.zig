cbfca397bcfe9c586f246374f16ec58b439d3e12
Kamil Tomsik <info@tomsik.cz>
2023-05-04 12:19:43 +0200

    support loading different tensor types

--- src/safetensors.zig
+++ src/safetensors.zig
@@ -10,6 +10,7 @@ pub const SafeTensors = struct {
 
 pub const TensorMapping = struct {
     tensor: *ggml.ggml_tensor,
+    type: ggml.ggml_type,
     start: usize,
     end: usize,
 };
@@ -45,7 +46,18 @@ pub fn safetensors_header(file: *SafeTensors) []const u8 {
 pub fn safetensors_load_tensors(file: *SafeTensors, mappings: []const TensorMapping) !void {
     // go through the mappings and set the data pointers
     for (mappings, 0..) |m, i| {
-        if (ggml.ggml_nbytes(m.tensor) != (m.end - m.start)) {
+        // create a copy with correct data type
+        var copy = m.tensor.*;
+        copy.type = m.type;
+        copy.data = file.data[m.start..m.end].ptr;
+        copy.nb = std.mem.zeroes(@TypeOf(copy.nb));
+        copy.nb[0] = ggml.ggml_type_size(m.type);
+        copy.nb[1] = copy.nb[0] * @intCast(usize, @divTrunc(copy.ne[0], @as(i64, ggml.ggml_blck_size(m.type))));
+        for (2..copy.nb.len) |bi| {
+            copy.nb[bi] = copy.nb[bi - 1] * @intCast(usize, copy.ne[bi - 1]);
+        }
+
+        if (ggml.ggml_nbytes(&copy) != (m.end - m.start)) {
             std.debug.print("tensor #{d} {s}{any} has size {d} it should be {d}\n", .{
                 i,
                 ggml.ggml_type_name(m.tensor.type),
@@ -56,7 +68,8 @@ pub fn safetensors_load_tensors(file: *SafeTensors, mappings: []const TensorMapp
             return error.TensorSizeMismatch;
         }
 
-        m.tensor.data = file.data[m.start..m.end].ptr;
+        // write the copy back
+        m.tensor.* = copy;
     }
 }
 
