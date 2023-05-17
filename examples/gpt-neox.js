b5f01724d0e9d85effa9127cac40c6e1127ea9b0
Kamil Tomsik <info@tomsik.cz>
2023-05-18 00:41:56 +0200

    more ops, gpt-neox is close(r)

--- examples/gpt-neox.js
+++ examples/gpt-neox.js
@@ -2,12 +2,22 @@
 //
 // this does not work yet!
 
+import { Context } from 'ggml-js/core'
 import { GPTNeoX } from 'ggml-js/llms'
 import { BPETokenizer } from 'ggml-js/tokenizers'
 
 // Load the model and tokenizer
 const model = GPTNeoX.loadFromFile(process.argv[2])
 const tokenizer = BPETokenizer.loadFromFile('./20B_tokenizer.json')
+
+// TODO
+const ctx = Context.init()
+const x = ctx.newTensor('i32', 1).set(0, 12092) // Hello
+const y = model.forward(x)
+const graph = ctx.buildForward(y)
+graph.compute()
+
+console.log(y.argmax()) // Should be 13
 
 // Generate text and print it one token at a time
 for (const t of model.generate(tokenizer.encode('\nHello world!'))) {
