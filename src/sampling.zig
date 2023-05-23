9b91d33bd39b8f5c880839f13d5122bfb1b22c81
Kamil Tomsik <info@tomsik.cz>
2023-05-23 09:48:28 +0200

    refactor

--- src/sampling.zig
+++ src/sampling.zig
@@ -22,23 +22,23 @@ pub fn sample_top_k_top_p(probs_tensor: *ggml.ggml_tensor, top_k: u32, top_p: f3
 
     var sorted_probs = try root.allocator.dupe(f32, probs);
     defer root.allocator.free(sorted_probs);
-
     std.sort.sort(f32, sorted_probs, {}, std.sort.desc(f32));
-    var cutoff: f32 = 0;
-    var cumsum: f32 = 0;
 
     // find cutoff value (either top_k or top_p)
-    for (0..top_k, sorted_probs) |_, p| {
-        cutoff = p;
+    var cumsum: f32 = 0;
+    for (sorted_probs, 1..) |p, i| {
         cumsum += p;
 
-        if (cumsum >= top_p) break;
-    }
-
-    // erase everything below
-    for (probs) |*p| {
-        if (p.* < cutoff) p.* = 0;
+        if (i == top_k or cumsum >= top_p) {
+            break cut_off(probs, p);
+        }
     }
 
     return @truncate(u32, random.weightedIndex(f32, probs));
+}
+
+fn cut_off(probs: []f32, cutoff: f32) void {
+    for (probs) |*p| {
+        if (p.* < cutoff) p.* = 0;
+    }
 }
