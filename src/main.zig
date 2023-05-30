60215753fb03c0ae5d147d93f3bbb212c588d262
Kamil Tomsik <info@tomsik.cz>
2023-05-30 19:07:56 +0200

    update napigen, cleanup bindings

--- src/main.zig
+++ src/main.zig
@@ -14,39 +14,18 @@ comptime {
 fn initModule(js: *napigen.JsContext, exports: napigen.napi_value) !napigen.napi_value {
     @setEvalBranchQuota(100_000);
     inline for (comptime std.meta.declarations(ggml)) |d| {
-        // constants
-        if (comptime std.mem.startsWith(u8, d.name, "GGML_")) {
-            if (comptime std.mem.eql(u8, d.name, "GGML_RESTRICT")) continue;
-            if (comptime std.mem.eql(u8, d.name, "GGML_ASSERT")) continue;
+        if (comptime !std.ascii.startsWithIgnoreCase(d.name, "ggml_") or
+            std.mem.eql(u8, d.name, "GGML_RESTRICT") or
+            std.mem.eql(u8, d.name, "GGML_ASSERT") or
+            std.mem.eql(u8, d.name, "ggml_internal_get_quantize_fn") or
+            @TypeOf(@field(ggml, d.name)) == type) continue;
 
-            try js.setNamedProperty(exports, d.name ++ "", try js.write(@field(ggml, d.name)));
-        }
-
-        // functions
-        if (comptime std.mem.startsWith(u8, d.name, "ggml_")) {
-            if (comptime std.mem.eql(u8, d.name, "ggml_internal_get_quantize_fn")) continue;
-
-            const T = @TypeOf(@field(ggml, d.name));
-
-            if (@typeInfo(T) == .Fn) {
-                try js.setNamedProperty(exports, d.name ++ "", try js.createNamedFunction(d.name ++ "", @field(ggml, d.name)));
-            }
-        }
+        try js.exportOne(exports, d.name, @field(ggml, d.name));
     }
 
     // extensions
-    inline for (comptime std.meta.declarations(safetensors)) |d| {
-        if (comptime std.mem.startsWith(u8, d.name, "safetensors_")) {
-            try js.setNamedProperty(exports, d.name ++ "", try js.createNamedFunction(d.name ++ "", @field(safetensors, d.name)));
-        }
-    }
-
-    // extensions
-    inline for (comptime std.meta.declarations(sampling)) |d| {
-        if (comptime std.mem.startsWith(u8, d.name, "sample_")) {
-            try js.setNamedProperty(exports, d.name ++ "", try js.createNamedFunction(d.name ++ "", @field(sampling, d.name)));
-        }
-    }
+    try js.exportAll(exports, safetensors);
+    try js.exportAll(exports, sampling);
 
     return exports;
 }
